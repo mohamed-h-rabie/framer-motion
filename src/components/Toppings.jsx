126a4225e324eb8bed4b04f84ec073dabadca7bb
mohamed-h-rabie <mohamed.rabie@marses.systems>
2025-01-18 21:33:24 +0200

    installing packages

---
+++ src/components/Toppings.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {  NavLink } from "react-router-dom";
+
+const Toppings = ({ addTopping, pizza }) => {
+  let toppings = [
+    "mushrooms",
+    "peppers",
+    "onions",
+    "olives",
+    "extra cheese",
+    "tomatoes",
+  ];
+
+  return (
+    <div className="toppings container">
+      <h3>Step 2: Choose Toppings</h3>
+      <ul>
+        {toppings.map((topping) => {
+          let spanClass = pizza.toppings.includes(topping) ? "active" : "";
+          return (
+            <li key={topping} onClick={() => addTopping(topping)}>
+              <span className={spanClass}>{topping}</span>
+            </li>
+          );
+        })}
+      </ul>
+
+      <NavLink to="/order">
+        <button>Order</button>
+      </NavLink>
+    </div>
+  );
+};
+
+export default Toppings;
